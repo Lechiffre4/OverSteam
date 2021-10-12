dc90f12e387232bff011edf5485a217feec477ee
lechiffre4 <toxcoa@gmail.com>
2021-10-12 12:53:05 +0200

    pages and some button

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
+var SignInRouter = require('./routes/signin');
 
 var app = express();
 
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/login', loginRouter);
+app.use('/signin', SignInRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
