73f16a40edfeb7ce7270a6d329a252075f35796d
lechiffre4 <toxcoa@gmail.com>
2021-10-12 13:04:47 +0200

    Login System views won't load

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
-var SignInRouter = require('./routes/signin');
+var signinRouter = require('./routes/signin');
 
 var app = express();
 
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
-app.use('/signin', SignInRouter);
+app.use('/signin', signinRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
