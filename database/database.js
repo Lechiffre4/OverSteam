908bd33e317421e573f1bdb93306525c72109595
lechiffre4 <toxcoa@gmail.com>
2021-10-12 11:42:12 +0200

    Project skeleton

--- database/database.js
+++ database/database.js
@@ -1,35 +1,20 @@
 const express = require('express'),
-  app = express(),
-  mysql = require('mysql'), // import mysql module
-  cors = require('cors'),
-  bodyParser = require('body-parser');
+	app = express(),
+	mysql = require('mysql'), // import mysql module
+	cors = require('cors'),
+	bodyParser = require('body-parser');
 const { Sequelize } = require('sequelize');
 
 // setup database
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  dialect: 'mysql',
+	host: process.env.DB_HOST,
+	dialect: 'mysql',
 });
-
-// make server object that contain port property and the value for our server.
-var server = {
-  port: 3306
-};
-
-// use the modules
-app.use(cors())
-app.use(bodyParser.json());
-app.use(express.static('public'));
-
-// starting the server
-
-app.listen(server.port, () => console.log(`Server started, listening port: ${server.port}`));
 
 // Database connection test
 try {
-  sequelize.authenticate();
-  console.log('Connection has been established successfully.');
+    sequelize.authenticate();
+    console.log('Connection has been established successfully.');
 } catch (error) {
-  console.error('Unable to connect to the database:', error);
+    console.error('Unable to connect to the database:', error);
 }
-
