6b99215c30a72949c35e30e4c1c54a1e92f317bd
Mathéo <matheo.galu56@gmail.com>
2021-10-26 16:44:30 +0200

    api router

--- model/user.js
+++ model/user.js
@@ -1,8 +1,18 @@
-const { Sequelize, Model, DataTypes } = require("sequelize");
-const sequelize = new Sequelize("sqlite::memory:");
-
-const User = sequelize.define("user", {
-    id: DataTypes.INTEGER,
-    name: DataTypes.TEXT,
-    age: DataTypes.INTEGER,
-});
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+    var User = sequelize.define('User', {
+        email: DataTypes.STRING,
+        username: DataTypes.STRING,
+        password: DataTypes.STRING,
+        bio: DataTypes.STRING,
+        isAdmin: DataTypes.BOOLEAN
+    }, {
+        classMethods: {
+            associate: function (models) {
+                // associations can be defined here
+                models.User.hasMany(models.Message);
+            }
+        }
+    });
+    return User;
+};
---
+++ routes/apiRouter.js
@@ -0,0 +1,24 @@
+// Imports
+var express = require('express');
+var usersCtrl = require('../controller/userController');
+
+// Router
+exports.router = (function () {
+    var apiRouter = express.Router();
+
+    // Users routes
+    apiRouter.route('/users/register/').post(usersCtrl.register);
+    apiRouter.route('/users/login/').post(usersCtrl.login);
+    // apiRouter.route('../users/me/').get(usersCtrl.getUserProfile);
+    // apiRouter.route('../users/me/').put(usersCtrl.updateUserProfile);
+
+    // Messages routes
+    // apiRouter.route('/messages/new/').post(messagesCtrl.createMessage);
+    // apiRouter.route('/messages/').get(messagesCtrl.listMessages);
+
+    // Likes
+    // apiRouter.route('/messages/:messageId/vote/like').post(likesCtrl.likePost);
+    // apiRouter.route('/messages/:messageId/vote/dislike').post(likesCtrl.dislikePost);
+
+    return apiRouter;
+})();
