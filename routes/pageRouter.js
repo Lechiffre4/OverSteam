ea52af2245d2ce9cd9e0cc7f1bc918de1ac6701c
Mathéo <matheo.galu56@gmail.com>
2021-11-19 10:20:48 +0100

    SessionStorage not working

--- routes/pageRouter.js
+++ routes/pageRouter.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index');
+    // token validation
+    var token = sessionStorage.getItem('value');
+
+    if (token) {
+        try {
+            console.log(jwt.verify(token, process.env.SIGN_TOKEN));
+            res.render('home');
+        } catch (err) {
+            sessionStorage.removeItem('value');
+            res.render('index');
+        }
+    }
 });
 
 /* GET singin listing. */
