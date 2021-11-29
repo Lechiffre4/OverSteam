9eddd6b555a00860af0940c0b2277907dfeccaa0
lechiffre4 <toxcoa@gmail.com>
2021-11-29 17:28:50 +0100

    modified game model

--- model/game.js
+++ model/game.js
@@ -6,7 +6,7 @@ var Game = db.define('Game', {
     name: DataTypes.STRING,
     description: DataTypes.STRING,
     author: DataTypes.STRING,
-    link: DataTypes.STRING,
+    link: DataTypes.STRING
 }, {
     classMethods: {
         associate: function (models) {
