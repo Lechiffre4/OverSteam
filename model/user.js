896ba25b0bd1ded3ed621911d3c5882dd9a32877
lechiffre4 <toxcoa@gmail.com>
2021-11-29 17:29:21 +0100

    Revert "Merge remote-tracking branch 'origin/main' into main"
    This reverts commit 71a8803495a502902f2195cb919b1584ce88e746.

--- model/user.js
+++ model/user.js
@@ -1,16 +1,16 @@
 'use strict';
-module.exports = (sequelize, DataTypes) => {
-	var User = sequelize.define('User', {
-		email: DataTypes.STRING,
-		username: DataTypes.STRING,
-		password: DataTypes.STRING,
-		isAdmin: DataTypes.BOOLEAN
-	}, {
-		classMethods: {
-			associate: function (models) {
-				models.User.hasMany(models.Game);
-			}
-		}
-	});
-	return User;
-}
+const { DataTypes } = require('sequelize')
+const { db } = require('../database/database');
+
+var User = db.define('User', {
+    email: DataTypes.STRING,
+    username: DataTypes.STRING,
+    password: DataTypes.STRING,
+    isAdmin: DataTypes.BOOLEAN
+}, {
+    classMethods: {
+        associate: function (models) {
+            models.User.hasMany(models.Game);
+        }
+    }
+});
