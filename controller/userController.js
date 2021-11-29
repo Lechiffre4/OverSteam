84e20b70c2620c800a76f99e9bcf95846be5467c
lechiffre4 <toxcoa@gmail.com>
2021-11-29 11:45:14 +0100

    disconnect buttons

--- controller/userController.js
+++ controller/userController.js
@@ -2,7 +2,6 @@ var { db } = require('../database/database');
 var jwtUtils = require('../utils');
 var bcrypt = require('bcrypt');
 var asyncLib = require('async');
-const { Relog } = require('../utils');
 
 // Constants
 const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
