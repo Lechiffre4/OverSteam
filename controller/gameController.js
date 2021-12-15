49d997db246b194c864035015cb903bb763c53ae
Mathéo <matheo.galu56@gmail.com>
2021-12-15 19:46:35 +0100

    This fucking shity function of dead is now working

--- controller/gameController.js
+++ controller/gameController.js
@@ -40,76 +40,76 @@ module.exports = {
 
 	addGame: function (req, res) {
 
-        var name = req.body.name;
-        var desc = req.body.description;
-        var link = req.body.link;
-        var author = utils.getUserId(req.headers.authorization);
-        var category = req.body.category;
+		var name = req.body.name;
+		var desc = req.body.description;
+		var link = req.body.link;
+		var author = utils.getUserId(req.headers.authorization);
+		var category = req.body.category;
 
-        console.log("name: " + name);
-        console.log("desc: " + desc);
-        console.log("link: " + link);
-        console.log("author: " + author);
-        console.log("category: " + category);
+		console.log("name: " + name);
+		console.log("desc: " + desc);
+		console.log("link: " + link);
+		console.log("author: " + author);
+		console.log("category: " + category);
 
-        asyncLib.waterfall([
-            function (done) {
-                db.models.Game.findOne({
-                    attributes: ['name'],
-                    where: { name: name }
-                })
-                    .then(function (GameFound) {
-                        done(null, GameFound);
-                    })
-                    .catch(function (err) {
-                        return res.status(500).json({ 'error': 'Unable to verify game' });
-                    });
-            },
-            function (GameFound, done) {
-                if (!GameFound) {
-                    done(null, GameFound);
-                } else {
-                    return res.status(409).json({ 'error': 'This game already exist' });
-                }
-            },
-            function (GameFound, done) {
-                console.log(name);
-                var newGame = db.models.Game.create({
-                    name: name,
-                    description: desc,
-                    link: link,
-                    CategoryId: category,
-                    UserId: author,
-                })
-                    .then(function (newGame) {
-                        console.log("newGame");
-                        done(newGame);
-                    })
-                    .catch(function (err) {
-                        console.log(newGame);
-                        return res.status(500).json({ 'error': 'Cannot add this game' });
-                    });
-            }
-        ], function (newGame) {
-		if (newGame) {
-                return res.status(201).json({
-                    'game': newGame.id
-                });
-            } else {
-                return res.status(500).json({ 'error': 'Cannot add this game' });
-            }
-        });
-    },
+		asyncLib.waterfall([
+			function (done) {
+				db.models.Game.findOne({
+					attributes: ['name'],
+					where: { name: name }
+				})
+					.then(function (GameFound) {
+						done(null, GameFound);
+					})
+					.catch(function (err) {
+						return res.status(500).json({ 'error': 'Unable to verify game' });
+					});
+			},
+			function (GameFound, done) {
+				if (!GameFound) {
+					done(null, GameFound);
+				} else {
+					return res.status(409).json({ 'error': 'This game already exist' });
+				}
+			},
+			function (GameFound, done) {
+				console.log(name);
+				var newGame = db.models.Game.create({
+					name: name,
+					description: desc,
+					link: link,
+					CategoryId: category,
+					UserId: author,
+				})
+					.then(function (newGame) {
+						console.log("newGame");
+						done(newGame);
+					})
+					.catch(function (err) {
+						console.log(newGame);
+						return res.status(500).json({ 'error': 'Cannot add this game' });
+					});
+			}
+		], function (newGame) {
+			if (newGame) {
+				return res.status(201).json({
+					'game': newGame.id
+				});
+			} else {
+				return res.status(500).json({ 'error': 'Cannot add this game' });
+			}
+		});
+	},
 
 	addtoMyGames: function (req, res) {
 		//var name = utils.getUserId(req.headers.authorization);
-		var id= req.body.id;
+		var id = req.body.id;
 
 		var game = req.body.gameid;
 		asyncLib.waterfall([
 			function (done) {
 				db.models.Game.destroy({
-					where: { id: id}
+					where: { id: id }
 				})
 					.then(function (GameFound) {
 						done(null, GameFound);
@@ -154,71 +154,65 @@ module.exports = {
 		// get header
 		var user = utils.getUserId(req.headers.authorization);
 
-		asyncLib.waterfall([
-			function (done) {
-				db.models.User_Game.findAll({
-					attributes: ['GameId'],
-					where: { UserId: user }
-				})
-					.then(function (game) {
-						console.log('inside')
-						done(null, game);
-					})
-					.catch(function (err) {
-						console.log("err", err);
-						return res.status(500).json({ 'error': 'Unable to verify game 0' });
-					});
-			},
-			function (games, done) {
-				var gameIds = [];
-				games.forEach(function (game) {
-					gameIds.push(game.GameIds);
-				});
-				done(null, gameIds);
-			},
-			function (gameIds, done) {
-				var games = [];
-				gameIds.forEach(function (gameId) {
-					db.models.Game.findOne({
-						where: { id: gameId }
-					}).then(function (game) {
-						// Get the game category
-						db.models.Category.findOne({
-							where: { id: game.CategoryId }
-						}).then(function (category) {
-							// Get the game author
-							db.models.User.findOne({
-								where: { id: game.UserId }
-							}).then(function (author) {
-								games.push({
-									id: game.id,
-									name: game.name,
-									description: game.description,
-									link: game.link,
-									category: category.name,
-									author: author.username
-								});
-							})
-								.catch(function (err) {
-									return res.status(500).json({ 'error': 'Unable to verify game 1' });
-								});
-						});
-					})
-						.catch(function (err) {
-							return res.status(500).json({ 'error': 'Unable to verify game 2' });
-						});
-				});
-				console.log(games);
-				done(null, games);
-			},
-			{
-				funnction(games, done) {
-					return res.status(200).json(games);
+		var games = [];
+		var count = 0;
+
+		db.models.User_Game.findAll({
+			attributes: ['GameId'],
+			where: { UserId: user }
+		})
+			.then(function (gameIds) {
+				if (gameIds.length == 0) {
+					return res.status(201).json({ 'games': [] });
 				}
-			}
-		], function (err) {
-			console.log('nique', err)
-		});
-	},
-	
+				gameIds.forEach(function (gameId) {
+					//get game
+					db.models.Game.findOne({
+						where: { id: gameId.dataValues.GameId }
+					})
+						.then(function (game) {
+							game = {
+								id: game.dataValues.id,
+								name: game.dataValues.name,
+								description: game.dataValues.description,
+								link: game.dataValues.link,
+								category: game.dataValues.CategoryId,
+								author: game.dataValues.UserId,
+							}
+							// replace categoryId with category name
+							db.models.Category.findOne({
+								where: { id: game.category }
+							})
+								.then(function (category) {
+									game.category = category.dataValues.name;
+									// Replace authorId with author name
+									db.models.User.findOne({
+										where: { id: game.author }
+									})
+										.then(function (author) {
+											game.author = author.dataValues.username;
+											games.push(game);
+											count++;
+											if (count == gameIds.length) {
+												return res.status(201).json(games);
+											}
+										})
+										.catch(function (err) {
+											return res.status(500).json({ 'error': 'Unable to fetch author' });
+										});
+								})
+								.catch(function (err) {
+									return res.status(500).json({ 'error': 'cannot fetch category' });
+								});
+						})
+						.catch(function (err) {
+							return res.status(500).json({ 'error': 'Unable to verify game' });
+						});
+				}
+				);
+			})
+			.catch(function (err) {
+				return res.status(500).json({ 'error': 'Unable to fetch games' });
+			});
+	}
 };
